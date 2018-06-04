40a9dc608ab8531d7b5178a326a829b6271e40b4
PlutusCat <haiyang_online@sina.com>
2018-06-04 16:29:03 +0800

    add DateToolsKit

---
+++ SwiftToolsKit/SwiftToolsKit/Main/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  SwiftToolsKit
+//
+//  Created by PlutusCat on 2018/6/4.
+//  Copyright © 2018年 SwiftToolsKit. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+
+}
+
